48d9237f087cfc68a1b204f288a21c9b1e5fedd0
Rick Ryan <rickryan29.rr@gmail.com>
2023-03-17 07:37:05 +0800

    angular fire integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJHqwdDKve8b5VI1jcR5LMXjeC49JzxzA",
+    authDomain: "scc-library-db6d3.firebaseapp.com",
+    databaseURL: "https://scc-library-db6d3-default-rtdb.firebaseio.com",
+    projectId: "scc-library-db6d3",
+    storageBucket: "scc-library-db6d3.appspot.com",
+    messagingSenderId: "1019656622303",
+    appId: "1:1019656622303:web:782b26f876d24546d8596a",
+    measurementId: "G-K3032KR72C"
+  }
 };
 
 /*
