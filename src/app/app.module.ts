d3bc8b8baa67e90c07334e221e4d2bd6fa4aba62
Rick Ryan <rickryan29.rr@gmail.com>
2023-03-17 15:34:54 +0800

    another try workflow

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    AngularFireAnalyticsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDp-n3JPvXmCB1RRaXBrODB11YauEjeZ4Y",
+        authDomain: "opacscc-a73c7.firebaseapp.com",
+        databaseURL: "https://opacscc-a73c7-default-rtdb.firebaseio.com",
+        projectId: "opacscc-a73c7",
+        storageBucket: "opacscc-a73c7.appspot.com",
+        messagingSenderId: "514399829505",
+        appId: "1:514399829505:web:63cae12e2f15759e694a00",
+        measurementId: "G-BPS0HTXKS3"
+      }
+};
+
