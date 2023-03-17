c27057916660a5fc59fea874715acbed97c4cc35
Rick Ryan <rickryan29.rr@gmail.com>
2023-03-17 12:02:42 +0800

    setup workflow

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDp-n3JPvXmCB1RRaXBrODB11YauEjeZ4Y",
+        authDomain: "opacscc-a73c7.firebaseapp.com",
+        projectId: "opacscc-a73c7",
+        storageBucket: "opacscc-a73c7.appspot.com",
+        messagingSenderId: "514399829505",
+        appId: "1:514399829505:web:63cae12e2f15759e694a00",
+        measurementId: "G-BPS0HTXKS3"
+      }
+};
