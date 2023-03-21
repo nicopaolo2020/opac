bb7884463eb92e515db8a814b7ada0bb877f56aa
Rick Ryan <rickryan29.rr@gmail.com>
2023-03-21 22:36:04 +0800

    added navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var typeAnimation: any;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit(): void {
+    new typeAnimation();
+  }
 }
