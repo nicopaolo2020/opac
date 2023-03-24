29126cef5187ecdfd4f56023634021cb9ca89e83
Rick Ryan <rickryan29.rr@gmail.com>
2023-03-24 11:27:55 +0800

    changing color theme

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,73 +1,25 @@
 function typeAnimation(){
-    // var catalog = ['catalog','catalog','catalog'];
-    //         var catalogPush = [];
-    //         for(var c = 0; c < catalog.length; c++) {
-    //             catalogPush +=  catalog[c];
-    //         }
-    //         console.log(catalogPush);
-    //         catalog.forEach((element, index, array) => {
-    //             console.log(element);
-    //             console.log(index);
-    //             console.log(array);
-    //             // catalogPush.push(Object.keys(element))
-    //         });
+    var slides = $('.slides');
+    images = slides.find('img');
 
-    //         console.log((catalog)[0]);
+    images.each(function(i) {
+        $(this).attr('data-id', i+1);
+    });
 
-            var slides = $('.slides');
-			images = slides.find('img');
+    var typed = new Typed('.typed-words', {
+        strings: ['<b>Catalog</b>', '<b> Catalog</b>','<b>Catalog</b> '],
+            typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 4000,
+        startDelay: 1000,
+        loop: true,
+        showCursor: true,
+        onStringTyped: (arrayPos,self) => {
+            arrayPos++;
+            // console.log(arrayPos);
+            $('.slides img').removeClass('active');
+            $('.slides img[data-id="'+arrayPos+'"]').addClass('active');
+        }
 
-			images.each(function(i) {
-				$(this).attr('data-id', i + 1);
-			});
-
-            const data = [
-                { datavalue: 'Catalog' },
-                { datavalue: 'Catalog' },
-                { datavalue: 'Catalog' },
-              ];
-            const defaultSpeed = 100;
-            
-            // document.querySelectorAll('.typed-words').forEach(el => new Typed(el, {
-            //         strings: data.map(row => row.datavalue),
-            //         stringsElement: null,
-            //         typeSpeed: defaultSpeed,
-            //         backSpeed: 80,
-            //         backDelay: 3000,
-            //         startDelay: 1000,
-            //         loop: true,
-            //         showCursor: true,
-            //     /** before each string gets typed, we set the "typeSpeed" to "20" when the string index is "1" otherwise we set it to "100" (the "defaultSpeed" variable) */
-            //     // preStringTyped: (arrayPos, self) => self.typeSpeed = arrayPos == 1 ? 20 : defaultSpeed
-            //     preStringTyped: (arrayPos, self) => {
-            //         arrayPos++;
-            //         self.typeSpeed = arrayPos == 1 ? 20 : defaultSpeed;
-            //         console.log(arrayPos++);
-            //         $('.slides img').removeClass('active');
-            //         $('.slides img[data-id="'+arrayPos+'"]').addClass('active');
-            //     }
-            //   }));
-
-            // console.log(images);
-                var typed = new Typed('.typed-words', {
-                    strings: ["Catalog", "Catalog", "Catalog"],
-                    stringsElement: null,
-                    typeSpeed: 80,
-                    backSpeed: 80,
-                    backDelay: 2000,
-                    startDelay: 1000,
-                    loop: true,
-                    showCursor: true,
-                    preStringTyped: (arrayPos, self) => {
-                        arrayPos++;
-                        // self++;
-                        // console.log(arrayPos++);
-                        $('.slides img').removeClass('active');
-                        $('.slides img[data-id="'+arrayPos+'"]').addClass('active');
-                    }
-    
-                });
-			
-
-            
+    });       
 }
